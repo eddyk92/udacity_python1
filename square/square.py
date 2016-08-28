453157c83c641f7721fc2081908347161c36dbd8
Kevin Eddye <kevneddy@gmail.com>
2016-08-27 22:07:05 -0700

    Add circle and triangle and customize colors

--- square/square.py
+++ square/square.py
@@ -1,18 +1,29 @@
 import turtle
 
 def draw_square():
+	for i in range(1,5):
+		some_turtle.forward(100)
+		some_turtle.right(90)
+
+def draw_art():
 	window = turtle.Screen()
 	window.bgcolor("red")
 
 	brad = turtle.Turtle()
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
+	brad.shape("turtle")
+	brad.color("green")
+	brad.speed(3)
+	draw_square(brad)
 
+	angie = turtle.Turtle()
+	angie.shape("arrow")
+	angie.color("blue")
+	angie.circle(100)
 
-print draw_square()
+	jake = turtle.Turtle()
+	jake.color("yellow")
+	jake.triangle(120)
+
+	window.exitonclick()
+
+draw_art()
